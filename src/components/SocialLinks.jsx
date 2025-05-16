fefc66f76a78f730c1164680325d429d7eee6b8f
qijie25 <leowqijie25@gmail.com>
2025-05-16 13:52:43 +0800

    update resume link to relative path

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -42,7 +42,7 @@ const SocialLinks = () => {
                     Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href: "/Resume_LEOW_QI_JIE.pdf",
+            href: "Resume_LEOW_QI_JIE.pdf",
             style: "rounded-tr-md",
             download: true,
         },
